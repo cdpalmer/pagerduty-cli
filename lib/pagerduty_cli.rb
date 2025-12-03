2f330f2c304c123d10df621327b2569cf51aac6b
Cody Palmer <cody.palmer08@gmail.com>
2025-12-02 17:04:46 -0700

    Verify we have the token

--- lib/pagerduty_cli.rb
+++ lib/pagerduty_cli.rb
@@ -9,9 +9,29 @@ module PagerdutyCli
     include PagerdutyCli::Helper
     default_task :help
 
+    def self.exit_on_failure?
+      true
+    end
+
     desc "hello NAME", "say hello to NAME"
     def hello(name)
+      initialize_client
       print_to_user("Hello #{name}", :green)
+    end
+
+    private
+
+    def initialize_client
+      puts
+      print_to_user("initializing...", :yellow)
+      print_to_user("  - Verify api token is set.", :yellow)
+      if ENV["PD_API_TOKEN"].nil? || ENV["PD_API_TOKEN"].empty?
+        print_to_user("PD_API_TOKEN environment variable not set.", :red)
+        exit 1
+      end
+
+      print_to_user("Setup successful.", :green)
+      puts
     end
   end
 end
--- spec/pagerduty_cli_spec.rb
+++ spec/pagerduty_cli_spec.rb
@@ -3,18 +3,71 @@ require "spec_helper"
 RSpec.describe PagerdutyCli::CLI do
   let(:cli) { described_class.new }
 
-  describe "#hello" do
-    it "sends the correct message and color to the helper" do
-      expect(cli).to receive(:print_to_user).with("Hello world", :green)
-      cli.hello("world")
+  context "with PD_API_TOKEN set" do
+    before do
+      allow(ENV).to receive(:[]).and_call_original
+      allow(ENV).to receive(:[]).with("PD_API_TOKEN").and_return("test-token")
+    end
+
+    describe "#hello" do
+      around do |example|
+        original_stdout = $stdout
+        $stdout = File.open(File::NULL, "w")
+        example.run
+        $stdout = original_stdout
+      end
+
+      it "initializes and then runs the command" do
+        expect(cli).to receive(:print_to_user).with("initializing...", :yellow).ordered
+        expect(cli).to receive(:print_to_user).with("  - Verify api token is set.", :yellow).ordered
+        expect(cli).to receive(:print_to_user).with("Setup successful.", :green).ordered
+        expect(cli).to receive(:print_to_user).with("Hello world", :green).ordered
+        cli.hello("world")
+      end
+    end
+
+    describe "default task" do
+      it "shows the help message" do
+        expect do
+          cli.help
+        end.to output(/Commands:/).to_stdout
+      end
     end
   end
 
-  describe "default task" do
-    it "shows the help message" do
-      expect do
-        described_class.start([])
-      end.to output(/Commands:/).to_stdout
+  context "with PD_API_TOKEN NOT set" do
+    before do
+      allow(ENV).to receive(:[]).and_call_original
+      allow(ENV).to receive(:[]).with("PD_API_TOKEN").and_return(nil)
+    end
+
+    describe "running a command" do
+      around do |example|
+        original_stdout = $stdout
+        $stdout = File.open(File::NULL, "w")
+        example.run
+        $stdout = original_stdout
+      end
+
+      it "exits with an error message" do
+        expect(cli).to receive(:print_to_user).with("initializing...", :yellow).ordered
+        expect(cli).to receive(:print_to_user).with("  - Verify api token is set.", :yellow).ordered
+        expect(cli).to receive(:print_to_user).with("PD_API_TOKEN environment variable not set.", :red).ordered
+
+        expect do
+          cli.hello("world")
+        end.to raise_error(SystemExit) do |e|
+          expect(e.status).to eq(1)
+        end
+      end
+    end
+
+    describe "running help command" do
+      it "does not exit and shows help" do
+        expect do
+          cli.help
+        end.to output(/Commands:/).to_stdout
+      end
     end
   end
 end
